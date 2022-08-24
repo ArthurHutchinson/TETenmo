1ec73dc3fb6392611cfd2526c881391c9aecbb10
Andrew McFate <andrew.mcfate@gmail.com>
2022-08-24 15:51:21 -0400

    created transfer table in tenmodb

--- tenmo/database/tenmo.sql
+++ tenmo/database/tenmo.sql
@@ -1,8 +1,8 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS tenmo_user, account;
+DROP TABLE IF EXISTS tenmo_user, account, transfer;
 
-DROP SEQUENCE IF EXISTS seq_user_id, seq_account_id;
+DROP SEQUENCE IF EXISTS seq_user_id, seq_account_id, seq_transfer_id;
 
 -- Sequence to start user_id values at 1001 instead of 1
 CREATE SEQUENCE seq_user_id
@@ -32,6 +32,21 @@ CREATE TABLE account (
 	CONSTRAINT PK_account PRIMARY KEY (account_id),
 	CONSTRAINT FK_account_tenmo_user FOREIGN KEY (user_id) REFERENCES tenmo_user (user_id)
 );
+
+CREATE SEQUENCE seq_transfer_id
+  INCREMENT BY 1
+  START WITH 3001
+  NO MAXVALUE;
+  
+ CREATE TABLE transfer (
+ 	transfer_id int NOT NULL DEFAULT nextval('seq_transfer_id'),
+  	from_account_id int NOT NULL,
+ 	to_account_id int NOT NULL,
+	transfer_amount decimal(13,2) NOT NULL,
+ 	CONSTRAINT PK_transfer PRIMARY KEY (transfer_id),
+	CONSTRAINT FK_from_account_id FOREIGN KEY (from_account_id) REFERENCES account (account_id),
+	CONSTRAINT FK_to_account_id FOREIGN KEY (to_account_id) REFERENCES account (account_id)
+  );
 
 
 COMMIT;
