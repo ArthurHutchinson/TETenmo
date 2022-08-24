cedadf57021bb841de70330070306c4e052d1539
Arthur Hutchinson <arthur.y.hutchinson@gmail.com>
2022-08-24 10:54:55 -0400

    Worked partially on Capstone and pushing to help partner reset code

--- tenmo/server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
+++ tenmo/server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
@@ -1,4 +1,26 @@
 package com.techelevator.tenmo.dao;
 
-public class JdbcAccountDao {
+import com.techelevator.tenmo.model.Account;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+public class JdbcAccountDao implements AccountDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcAccountDao (DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Account createAccount(Account newAccount) {
+        String sql = "";
+        Integer newId = jdbcTemplate.queryForObject(sql, Integer.class);
+        newAccount.setAccountId(newId);
+        return newAccount;
+    }
+
+
+
 }
