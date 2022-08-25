36f7b3f092c832482f2ba534aedf2e7593679659
Andrew McFate <andrew.mcfate@gmail.com>
2022-08-25 10:55:53 -0400

    passing the baton

--- tenmo/server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
+++ tenmo/server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDao.java
@@ -19,12 +19,10 @@ public class JdbcTransferDao implements TransferDao{
 
 @Override
     public Transfer createTransfer(Transfer newTransfer) {
-//        User user = null;
-//        User user2 = null;
-//        Account account = null;
-//        Account account2 = null;
+
         String sql = "INSERT INTO transfer (from_user_id, from_account_id, to_user_id, to_account_id, transfer_amount)\n" +
                 "VALUES ((SELECT user_id FROM tenmo_user WHERE user_id = ?),?,?,?,?) RETURNING transfer_id;";
+        String sql2 = "UPDATE account SET "
         Integer newId = jdbcTemplate.queryForObject(sql, Integer.class,
                 newTransfer.getFromUserId() , newTransfer.getFromAccountId(),
                 newTransfer.getToUserId(), newTransfer.getToAccountId(),
